c370322f38e080b795753dbb8ff7689df50955fb
Valera Kamelkov <tahaburrent@gmail.com>
2023-07-11 00:18:24 +0200

    ~

--- routes/api/ip.js
+++ routes/api/ip.js
@@ -1,10 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const router = express.Router();
 
+const IpShema = new mongoose.Schema({
+  ip: { type: String, unique: true, required: [true, "Set Ip"] },
+});
+
+const Ip = mongoose.model("user", IpShema);
+
 router.post("/", async (req, res, next) => {
-  console.log(req.body.ip);
-  res.send();
+  try {
+    console.log(req.body.ip);
+    await Ip.create({ ip: req.body.ip });
+    res.send();
+  } catch (error) {
+    res.send(error.message);
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 const app = require("./app");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-app.listen(3000);
+app.listen(3000, async () => {
+  try {
+    await mongoose.connect(process.env.MONG);
+    console.log("good");
+  } catch (error) {
+    console.log(error);
+  }
+});
