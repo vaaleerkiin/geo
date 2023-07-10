6d321abcec7f7e5ad3856d09a591492a1277c81e
Valera Kamelkov <tahaburrent@gmail.com>
2023-07-10 23:53:52 +0200

    ~

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 
-const contactsRouter = require("./routes/api/ip");
+const Router = require("./routes/api/ip");
 
 const app = express();
 
@@ -12,7 +12,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/ip", contactsRouter);
+app.use("/api/ip", Router);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
--- routes/api/ip.js
+++ routes/api/ip.js
@@ -1,17 +1,10 @@
 const express = require("express");
-const axios = require("axios");
+
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
-  const ips = (
-    req.headers["cf-connecting-ip"] ||
-    req.headers["x-real-ip"] ||
-    req.headers["x-forwarded-for"] ||
-    req.connection.remoteAddress ||
-    ""
-  ).split(",");
-  console.log(ips);
-  res.json({ ip: ips[0].trim() });
+router.post("/", async (req, res, next) => {
+  console.log(req.body.ip);
+  res.send();
 });
 
 module.exports = router;
